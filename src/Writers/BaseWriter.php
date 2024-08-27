005345400c7f540e57459dd2acbe881929355fba
VlGlad <80222832+VlGlad@users.noreply.github.com>
2024-08-27 16:49:27 +0500

    Add exception "Fail to create dir structure "

--- src/Writers/BaseWriter.php
+++ src/Writers/BaseWriter.php
@@ -10,6 +10,9 @@ abstract class BaseWriter implements WriterInterface
         if (!is_dir($file_path)) {
             mkdir($file_path, 655, true);
         }
+        if (!is_dir($file_path)) {
+            throw new \Exception('Failed to create dir structure');
+        }
     }
 
     abstract public function save(string $file_path, array $map_arr);
